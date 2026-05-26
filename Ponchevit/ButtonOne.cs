a4745cc84a1a16bdc046bcbc51edde44cd499026
Gabriel Contreras <gabcontrerasfr@gmail.com>
2026-05-25 22:01:38 -0400

    Proyecto de VS agregado

---
+++ Ponchevit/ButtonOne.cs
@@ -0,0 +1,16 @@
+﻿using Autodesk.Revit.UI;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.Attributes;
+
+namespace Ponchevit
+{
+    [Transaction(TransactionMode.ReadOnly)]
+    public class CommandOne : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            TaskDialog.Show("Command One", "Command One executed successfully.");
+            return Result.Succeeded;
+        }
+    }
+}
